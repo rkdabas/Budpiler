9f1d4726eb0a74e9601b934acb1a6d4266ebf8b5
rkdabas <rajatdabas05@gmail.com>
2024-07-20 17:06:55 +0530

    first commit

---
+++ src/test.js
@@ -0,0 +1,21 @@
+// this is how our data models will look like
+
+{
+  title: "folder",
+    id: "uniq_id",
+    files: [
+      {
+        id:'uniq_id',
+        title: 'index',
+        language: 'cpp',
+        code:'cout<<"hello";'
+      },
+      {
+        id:'uniq_id',
+        title: 'test',
+      language: 'java',
+      code:'"wassup'
+      }
+    ]
+      
+}
